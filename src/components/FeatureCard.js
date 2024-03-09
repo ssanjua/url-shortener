04348394f7fb9ae26d4f4892873ad01993aec7e4
paupallares <ppaupallares@gmail.com>
2024-03-09 19:18:38 -0300

    featureCard center

--- src/components/FeatureCard.js
+++ src/components/FeatureCard.js
@@ -5,8 +5,8 @@ const FeatureCard = ({ icon, title, description }) => {
   return (
     <div className={styles.card}>
       <img src={icon} alt={title} className={styles.icon} />
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
+      <h3 className={styles.titleCard}>{title}</h3>
+      <p className={styles.descriptionCard}>{description}</p>
     </div>
   );
 };
