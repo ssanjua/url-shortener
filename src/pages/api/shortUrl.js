2c0d558e57d5d33b28c09ae5b277cd9a87242e5b
paupallares <ppaupallares@gmail.com>
2024-04-18 20:10:08 +0200

    fix prisma call

--- src/pages/api/shortUrl.js
+++ src/pages/api/shortUrl.js
@@ -1,31 +1,27 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-export default async function handler (req, res) {
-  const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-  let { url } = req.body
+export default async function handler(req, res) {
+  let { url } = req.body;
 
-  // Asegurar que la URL comienza con http:// o https://
   if (!url.startsWith('http://') && !url.startsWith('https://')) {
     url = `http://${url}`;
   }
 
-  const shortUrl = Math.random()
-    .toString(36)
-    .substr(2, 6)
+  const shortUrl = Math.random().toString(36).substr(2, 6);
 
   try {
     let link = await prisma.link.findUnique({ where: { url } });
-    
+
     if (!link) {
       link = await prisma.link.create({
         data: { url, shortUrl }
       });
     }
-    prisma.$disconnect()
 
-    return res.status(200).send(link)
+    return res.status(200).send(link);
   } catch (error) {
-    return res.status(500).send(error)
+    return res.status(500).send(error);
   }
 }
